4ef9dd83c7253ccdc572ca160c4f3c7d15e88dd1
saitem <36552192+Saitem@users.noreply.github.com>
2021-07-27 11:44:52 +0300

    sign up promise

--- gateway/src/controllers/auth.controller.ts
+++ gateway/src/controllers/auth.controller.ts
@@ -1,22 +1,44 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { grpcInit } from '../grpc_init';
 
 const PROTO_PATH = __dirname + '/protos/auth.proto';
 
 const authService = grpcInit(PROTO_PATH, 'AuthService', '0.0.0.0:8888');
 
-export const signUp = async (req: Request, res: Response) => {
-  authService.SignUp({
-    username: req.body.username,
-    password: req.body.password
-  }, (err, response) => {
-    if (response) {
-      res.send({ ...response});
-    } else {
-      res.send(response);
-    }
-  });
+declare global {
+  namespace Express {
+      interface Request {
+        resp? : Record<string,any>
+      }
+  }
 }
+
+async function signUpPromise (req: Request, res: Response, retries: number = 3, timeout: number = 300)  {
+    const result = await new Promise((async (resolve, reject) => {
+      await authService.SignUp({
+        username: req.body.username,
+        password: req.body.password
+      }, (err, response) => {
+        if (!response) {
+          setTimeout(() => {
+            return signUpPromise(req, res, retries - 1, timeout * 2);
+
+          }, timeout)
+        } else {
+          return resolve(response)
+        }
+      })
+    }))
+
+    return result
+}
+
+export const signUp = async (req: Request, res: Response) => {
+  const response = await signUpPromise(req, res)
+  res.send(response)
+}
+
+
 
 export const signIn = async (req: Request, res: Response) => {
   authService.SignIn({
@@ -24,7 +46,7 @@ export const signIn = async (req: Request, res: Response) => {
     password: req.body.password
   }, (err, response) => {
     if (response) {
-      res.send({ ...response});
+      res.send({ ...response });
     } else {
       res.send(response);
     }
