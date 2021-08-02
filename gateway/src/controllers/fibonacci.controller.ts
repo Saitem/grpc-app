3bf5ca16828be4181a68138a5965dc53febd6bba
saitem <36552192+Saitem@users.noreply.github.com>
2021-08-02 11:36:20 +0300

    fibonacci route fix

--- gateway/src/controllers/fibonacci.controller.ts
+++ gateway/src/controllers/fibonacci.controller.ts
@@ -1,19 +1,15 @@
 import { Request, Response } from 'express';
 import { grpcInit } from '../grpc_init';
+import retryGrpcCall from '../helpers/retry';
 
 const PROTO_PATH = __dirname + '/protos/fibonacci.proto';
 
 const fibonacciService = grpcInit(PROTO_PATH, 'FibonacciService', '0.0.0.0:8000');
 
-
 export const fibonacci = async (req: Request, res: Response) => {
-  fibonacciService.Fibonacci({
+  const response = await retryGrpcCall(fibonacciService, 'Fibonacci', {
     num: req.body.num
-  }, (err, response) => {
-    if (response) {
-      res.send({ ...response});
-    } else {
-      res.send(response);
-    }
-  });
+  })
+
+  res.send(response);
 }
